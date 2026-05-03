33b553c24c63e816daaa1fe990fd4d28f3f52247
gangbu1234 <d.gangbu@gmail.com>
2026-05-03 20:21:37 +0900

    fix: アイコンの余白とキャッシュ問題を解決するためファイル名と設定を更新

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -7,8 +7,8 @@ export default function Document() {
         <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=0, viewport-fit=cover" />
         <meta name="apple-mobile-web-app-capable" content="yes" />
         <meta name="apple-mobile-web-app-status-bar-style" content="default" />
-        <link rel="icon" href="/favicon.png" />
-        <link rel="apple-touch-icon" href="/apple-touch-icon.png" />
+        <link rel="icon" href="/favicon-v2.png" />
+        <link rel="apple-touch-icon" href="/apple-touch-icon-v2.png" />
         <link rel="manifest" href="/manifest.json" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
